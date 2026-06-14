e265da919a0af4373674bb101b8ad97f9c1fcf4b
Eason Huang <easonh6444@gmail.com>
2026-06-14 16:00:25 +0800

    fix: add Cloud Run back to terraform with placeholder image

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,4 +103,30 @@ resource "google_artifact_registry_repository" "app" {
   format        = "DOCKER"
 }
 
+resource "google_cloud_run_v2_service" "app" {
+  name                = "myfirstweb"
+  location            = "asia-east1"
+  deletion_protection = false
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      ports {
+        container_port = 19191
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [template]
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_member" "public" {
+  name     = google_cloud_run_v2_service.app.name
+  location = google_cloud_run_v2_service.app.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
 
