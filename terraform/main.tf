3cbe0bf9da3a72132f96477e1718500f9a694a61
Eason Huang <easonh6444@gmail.com>
2026-06-21 22:36:31 +0800

    fix: Cloud Run use jenkins-deploy SA to avoid actAs permission issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -108,6 +108,7 @@ resource "google_cloud_run_v2_service" "app" {
   deletion_protection = false
 
   template {
+    service_account = google_service_account.jenkins_deploy.email
     containers {
       image = "asia-east1-docker.pkg.dev/ckc101-13/myfirstweb/myfirstweb:${var.app_image_tag}"
       ports {
